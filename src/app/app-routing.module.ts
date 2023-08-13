644c10ece59ab3bd32c4f0cd7f1c6451c160876b
NhanN <NhanN@fpt.com.vn>
2023-08-13 19:03:48 +0700

    add base initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  // {
+  //   path: '',
+  //   loadChildren: () =>
+  //     import('./iframe/iframe.module').then((m) => m.IframeModule),
+  // },
+  // { path: '403', component: PageAccessDeniedComponent },
+  // { path: '404', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
